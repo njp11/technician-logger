8695c672830f7f685e1bd94e47a89b4558af8afc
NiravPatel111 <48572469+NiravPatel111@users.noreply.github.com>
2019-09-11 17:48:59 -0400

    addLog action added and called from component

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -29,7 +29,8 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 };
 
 Logs.prototypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
