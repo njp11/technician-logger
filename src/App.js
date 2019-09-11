19a8aca3607118c1754cf4fcb79e5a0eb8d371f7
NiravPatel111 <48572469+NiravPatel111@users.noreply.github.com>
2019-09-11 16:32:23 -0400

    redux store, rootReducer and logAction created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import EditLogModal from './components/logs/EditLogModal';
 import AddTachModal from './components/techs/AddTechModal';
 import AddBtn from './components/layout/AddBtn';
 import 'materialize-css/dist/css/materialize.min.css';
+import { Provider } from 'react-redux';
+import store from './store';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import TechListModal from './components/techs/TechListModal';
 import './App.css';
@@ -16,17 +18,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTachModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTachModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
